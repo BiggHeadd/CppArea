79c374b4e1a62a4725a47f38cfc7ed06547afa9e
BiggHeadd <153405814@qq.com>
2019-09-07 00:08:08 +0800

    学会使用头文件和源代码编写C++

---
+++ helloworld/class_helloworld.cpp
@@ -0,0 +1,12 @@
+//
+// Created by bighead on 2019/9/6.
+//
+
+#include "class_helloworld.h"
+#include <iostream>
+#include <cstdint>
+
+void class_helloworld::speak() {
+    std::cout << "Hello World!" << "\n";
+    std::cout << "From class_helloworld";
+}
---
+++ helloworld/class_helloworld.h
@@ -0,0 +1,14 @@
+//
+// Created by bighead on 2019/9/6.
+//
+
+#ifndef HELLOWORLD_CLASS_HELLOWORLD_H
+#define HELLOWORLD_CLASS_HELLOWORLD_H
+
+class class_helloworld{
+public:
+    void speak();
+};
+
+
+#endif //HELLOWORLD_CLASS_HELLOWORLD_H
