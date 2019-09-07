5662211ea4c5c11a08c16920fcff795195c7aeb7
BiggHeadd <153405814@qq.com>
2019-09-08 01:29:56 +0800

    第二个ppt的作业前半部分

---
+++ 1/Homework.h
@@ -0,0 +1,19 @@
+//
+// Created by bighead on 2019/9/8.
+//
+
+#ifndef INC_1_HOMEWORK_H
+#define INC_1_HOMEWORK_H
+
+
+class Homework {
+
+private:
+    long long seconds;
+
+public:
+    void seconds2DHMS();
+};
+
+
+#endif //INC_1_HOMEWORK_H
