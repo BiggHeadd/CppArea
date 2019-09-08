b7c2dd9b90ba4d9045e94b7c3864fba3bcb89458
BiggHeadd <153405814@qq.com>
2019-09-08 16:55:02 +0800

    上课教材第二章的练习题（枚举类型）

---
+++ TsingHua/2/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    // 2-33
+//    enum Weekdays{Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday};
+//    int day = Monday;
+//    std::cout<<day;
+
+    // 2-34
+//    enum Balls {Red, Yellow, Blue, White, Black};
+//    int one, two, three;
+//    int count=0;
+//    for(one=Red; one<=Black; one++){
+//        for(two=one+1; two<=Black; two++){
+//            if(one != two){
+//                for(three=two+1; three<=Black; three++){
+//                    if(one!=three and two!=three){
+//                        count+=1;
+//                    }
+//                }
+//            }
+//        }
+//    }
+//    std::cout<<count;
+
+    // 2-35
+//    enum MulTable {one=1, two, three, four, five, seven, eight, nine};
+//    int first, second;
+//    std::string printSample;
+//    for(first=one; first<=nine; first++){
+//        for(second=one; second<=nine; second++){
+//            std::cout.setf(std::ios::left);
+//            std::cout.width(2);
+//            std::cout<<second<<" * ";
+//            std::cout.setf(std::ios::left);
+//            std::cout.width(2);
+//            std::cout<<first<<" = ";
+//            std::cout.setf(std::ios::left);
+//            std::cout.width(2);
+//            std::cout<<first*second<<"\t";
+//        }
+//        std::cout<<std::endl;
+//    }
+
+    return 0;
+}
