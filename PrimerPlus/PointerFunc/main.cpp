c7257b37df8768f1816110913659c6ddb1ac9863
BiggHeadd <153405814@qq.com>
2019-09-16 21:46:07 +0800

    引用变量的使用

--- PrimerPlus/PointerFunc/main.cpp
+++ PrimerPlus/PointerFunc/main.cpp
@@ -23,7 +23,24 @@ void estimate(double *(*pf)(int)){
 }
 
 int main() {
+    // 函数指针
     estimate(pam);
     estimate(pf);
+
+    // 引用变量
+    int rate = 3;
+    int& rate_ = rate;
+
+    std::cout<<"rate: "<<rate<<std::endl;
+    std::cout<<"rate_: "<<rate_<<std::endl<<std::endl;
+
+    rate++;
+
+    std::cout<<"rate: "<<rate<<std::endl;
+    std::cout<<"rate_: "<<rate_<<std::endl<<std::endl;
+
+    std::cout<<"address rate: "<<&rate<<std::endl;
+    std::cout<<"address rate_: "<<&rate_<<std::endl;
+
     return 0;
 }
