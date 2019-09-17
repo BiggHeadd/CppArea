bab047cb87bb117564db7d84f253218a895ab0d3
BiggHeadd <153405814@qq.com>
2019-09-17 11:54:45 +0800

    结构体引用的使用

--- PrimerPlus/PointerFunc/main.cpp
+++ PrimerPlus/PointerFunc/main.cpp
@@ -22,25 +22,56 @@ void estimate(double *(*pf)(int)){
     (*pf)(1);
 }
 
+struct free_throws{
+    std::string name;
+    int made{};
+    int miss{};
+    float percent{};
+};
+
+void set_ft(free_throws& ft){
+    std::cout<<"Please enter name: ";
+    std::cin>>ft.name;
+    std::cout<<"made: ";
+    std::cin>>ft.made;
+    std::cout<<"missed: ";
+    std::cin>>ft.miss;
+
+    ft.percent = ft.made/((ft.made+ft.miss)*1.0);
+}
+
+void print_ft(const free_throws& ft){
+    std::cout<<"name: "<<ft.name<<std::endl;
+    std::cout<<"made: "<<ft.made<<std::endl;
+    std::cout<<"miss: "<<ft.miss<<std::endl;
+    std::cout<<"percent: "<<ft.percent<<std::endl;
+}
+
 int main() {
     // 函数指针
-    estimate(pam);
-    estimate(pf);
+//    estimate(pam);
+//    estimate(pf);
 
     // 引用变量
-    int rate = 3;
-    int& rate_ = rate;
+//    int rate = 3;
+//    int& rate_ = rate;
+//
+//    std::cout<<"rate: "<<rate<<std::endl;
+//    std::cout<<"rate_: "<<rate_<<std::endl<<std::endl;
+//
+//    rate++;
+//
+//    std::cout<<"rate: "<<rate<<std::endl;
+//    std::cout<<"rate_: "<<rate_<<std::endl<<std::endl;
+//
+//    std::cout<<"address rate: "<<&rate<<std::endl;
+//    std::cout<<"address rate_: "<<&rate_<<std::endl;
 
-    std::cout<<"rate: "<<rate<<std::endl;
-    std::cout<<"rate_: "<<rate_<<std::endl<<std::endl;
-
-    rate++;
-
-    std::cout<<"rate: "<<rate<<std::endl;
-    std::cout<<"rate_: "<<rate_<<std::endl<<std::endl;
-
-    std::cout<<"address rate: "<<&rate<<std::endl;
-    std::cout<<"address rate_: "<<&rate_<<std::endl;
+    // 结构体引用
+    std::cout<<"set up free throw struct"<<std::endl;
+    free_throws ft;
+    set_ft(ft);
+    print_ft(ft);
 
     return 0;
 }
