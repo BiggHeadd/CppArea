5346c62e8e3a3208dcea6b90edbf2b6ccb04e4eb
BiggHeadd <153405814@qq.com>
2019-09-18 18:37:09 +0800

    p298 第二题 实现

--- PrimerPlus/3/main.cpp
+++ PrimerPlus/3/main.cpp
@@ -14,11 +14,40 @@ void printString(const char* string, int count){
     }
 }
 
+struct CandyBar{
+    std::string brandName;
+    double weight{};
+    int energy{};
+};
+
+void setCandyBar(CandyBar& candyBar, const char* brandName="Millennium Munch", double w=2.85, int e=350);
+void SetCandyBar(CandyBar& candyBar, char* brandName, double w, int e){
+    candyBar.brandName = brandName;
+    candyBar.weight = w;
+    candyBar.energy = e;
+}
+
+void printCandyBar(const CandyBar& candyBar){
+    std::cout<<std::endl;
+    std::cout<<"**********The info of this CandyBar**********"<<std::endl;
+    std::cout<<"Name: "<<candyBar.brandName<<std::endl;
+    std::cout<<"Weight: "<<candyBar.weight<<"\t"<<"Engry: "<<candyBar.energy<<std::endl;
+    std::cout<<std::endl;
+}
+
+
 int main() {
-    const char* a = "aaaaa";
-    for(int j=0; j<10; j++){
-        printString(a, j);
-    }
-    printString(a);
+    // p298 1.
+//    const char* a = "aaaaa";
+//    for(int j=0; j<10; j++){
+//        printString(a, j);
+//    }
+//    printString(a);
+
+    // p298 2.
+    CandyBar candyBar;
+    char* name = "chocolate";
+    SetCandyBar(candyBar, name, 3.00, 400);
+    printCandyBar(candyBar);
     return 0;
 }
